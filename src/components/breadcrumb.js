f52f781a5aec78da569749885d880adc448b3f9e
Manuel <manu.12.d@gmail.com>
2024-03-19 14:59:17 -0400

    Fix

--- src/components/breadcrumb.js
+++ src/components/breadcrumb.js
@@ -9,7 +9,7 @@ const Breadcrumbs = ({ items, className, classNameLi }) => {
         {items.map((item, index) => (
           <li key={index}>
             {item.link ? (
-              <a className={classNameLi}>{item.label}</a>
+              <a href="" className={classNameLi}>{item.label}</a>
             ) : (
               <span>{item.label}</span>
             )}
